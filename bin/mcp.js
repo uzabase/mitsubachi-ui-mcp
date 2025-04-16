de619e7914fc06bc492abd16b3a8f41f92795c64
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2025-04-17 01:56:20 +0900

    a

--- bin/mcp.js
+++ bin/mcp.js
@@ -2,6 +2,7 @@
 
 // src/index.ts
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
 // src/manifest.ts
 import custom from "mitsubachi-ui/custom-elements.json" with { type: "json" };
@@ -15,6 +16,9 @@ var CustomElementJson = class {
   }
   get summary() {
     return this.raw.summary;
+  }
+  stringify() {
+    return JSON.stringify(this.raw);
   }
   describe(attribute) {
     for (let { name, description } of this.raw.attributes) {
@@ -65,50 +69,6 @@ function loadDefaultManifest() {
 }
 
 // src/index.ts
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-
-// src/tool.ts
-function makeWebComponentContent(manifest) {
-  const res = [];
-  for (const [tagName, summary] of Object.entries(manifest.summaries)) {
-    res.push({ type: "text", text: `\u30AB\u30B9\u30BF\u30E0\u8981\u7D20 <${tagName}> ${summary}` });
-  }
-  return res;
-}
-
-// src/sp-logo.ts
-import { z } from "zod";
-
-// src/schema.ts
-function describe(schema, customElement) {
-  for (const [attributeName, value] of Object.entries(schema)) {
-    if (!schema[attributeName].description) {
-      const description = customElement.describe(attributeName);
-      if (description) {
-        schema[attributeName] = value.describe(description);
-      }
-    }
-  }
-}
-
-// src/sp-logo.ts
-import { html, nothing } from "lit-html";
-function getSpLogoDefinition(customElement) {
-  const input = { language: z.enum(["ja", "en", "zh"]) };
-  describe(input, customElement);
-  return [
-    input,
-    ({ language }) => {
-      return html`<sp-logo language=${language || nothing}></sp-logo>`;
-    }
-  ];
-}
-
-// src/index.ts
-import { render } from "@lit-labs/ssr";
-import {
-  collectResult
-} from "@lit-labs/ssr/lib/render-result.js";
 function buildMcpServer() {
   return new McpServer({
     name: "mitsubachi-mcp",
@@ -120,39 +80,23 @@ function buildMcpServer() {
   });
 }
 function defineTools(server, manifest) {
-  const content = makeWebComponentContent(manifest);
+  const customElements = manifest.customElements;
+  const texts = [];
+  for (const [tag, elm] of Object.entries(customElements)) {
+    texts.push(`<${tag}>\u306Ecustom element manifest: ${elm.stringify()}`);
+  }
   server.tool(
     "mitsubachi-ui-web-components",
-    "mitsubachi-ui\u306E\u30AB\u30B9\u30BF\u30E0\u8981\u7D20\u306E\u4E00\u89A7\u3092\u63D0\u4F9B\u3057\u307E\u3059\u3002",
+    "mitsubachi-ui\u306Ecustom-elements.json\u3092\u63D0\u4F9B\u3057\u307E\u3059\u3002",
     {},
     async () => {
       return {
-        content
+        content: texts.map((text) => {
+          return { type: "text", text };
+        })
       };
     }
   );
-  for (const { tag, body } of [{ tag: "sp-logo", body: getSpLogoDefinition }]) {
-    const customElement = manifest.customElements[tag];
-    if (customElement) {
-      const [input, builder] = body(customElement);
-      server.tool(
-        `mitsubachi-${tag}`,
-        customElement.summary ?? `<${tag}>\u3092\u751F\u6210\u3057\u307E\u3059\u3002`,
-        input,
-        async (shape) => {
-          const rendered = await collectResult(render(builder(shape)));
-          return {
-            content: [
-              {
-                type: "text",
-                text: rendered
-              }
-            ]
-          };
-        }
-      );
-    }
-  }
 }
 async function main() {
   const server = buildMcpServer();
